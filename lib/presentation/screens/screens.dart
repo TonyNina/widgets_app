8c99199a688ed63eab6c0c8210a8cf6434f7e043
Tony Nina <ninatony05@gmail.com>
2023-10-10 10:55:02 -0500

    Button screen finished

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,6 +1,6 @@
 
 //Este archivo se va encargar de exportar todo lo que son mis SCREENS
-export 'package:widgets_app/presentation/screens/buttons/buttons.dart';
+export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
